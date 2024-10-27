ed8b80077103426021b89fbac1ac903d48e27a3d
rayiqi <ramadifan769@gmail.com>
2024-10-27 12:58:42 +0700

    ok

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully');
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1); // Stop the application if connection fails
+  }
+};
+
 export default connectDB;
